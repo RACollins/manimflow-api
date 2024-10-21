1dbea2b426eafac795b3e4333eb9a7112725575f
RACollins <richardcollins128@mail.com>
2024-10-21 23:36:03 +0900

    Added a copy to clipboard button

--- manimflow-site/components/results.tsx
+++ manimflow-site/components/results.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import Editor from "@monaco-editor/react";
+import React, { useRef } from "react";
+import Editor, { OnMount } from "@monaco-editor/react";
+import { FaRegCopy } from "react-icons/fa";
+import { editor } from "monaco-editor";
 
 interface ResultsProps {
   prompt: string;
@@ -8,13 +10,33 @@ interface ResultsProps {
 }
 
 const Results: React.FC<ResultsProps> = (props) => {
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+
+  const handleEditorDidMount: OnMount = (editor) => {
+    editorRef.current = editor;
+  };
+
+  const handleCopy = () => {
+    if (editorRef.current) {
+      const content = editorRef.current.getValue();
+      navigator.clipboard
+        .writeText(content)
+        .then(() => {
+          alert("Code copied to clipboard!");
+        })
+        .catch((err) => {
+          console.error("Failed to copy: ", err);
+        });
+    }
+  };
+
   return (
-    <>
+    <div className="relative">
       <p>Prompt</p>
       <div className="bg-white p-2 w-full rounded-md text-slate-700 my-3">
         {props.prompt}
       </div>
-      <div className="mt-2">
+      <div className="mt-2 relative">
         <Editor
           height="30vh"
           defaultLanguage="python"
@@ -24,13 +46,21 @@ const Results: React.FC<ResultsProps> = (props) => {
           }}
           theme="vs-dark"
           defaultValue={props.generatedCode}
+          onMount={handleEditorDidMount}
         />
+        <button
+          className="absolute bottom-2 right-5 z-10 bg-opacity-0 text-slate-300 p-1 rounded-md hover:bg-teal-100 hover:text-slate-600 transition-colors"
+          onClick={handleCopy}
+          title="Copy code"
+        >
+          <FaRegCopy size={14} />
+        </button>
       </div>
 
       <button className="generic-btn" onClick={props.onBack}>
         Back
       </button>
-    </>
+    </div>
   );
 };
 
